a8d7c481b9e94e33e08f432a6db31814f7b184cb
Matt Jin <57683147+MattJin19@users.noreply.github.com>
2022-01-01 17:42:25 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
 from dash import dcc, html
-
+from flask import Flask
 
 def figure_1():
     layout=go.Layout(title='Title',)
     fig=go.Figure(data=[go.Bar(go.Bar(y=[2, 1, 3]))],layout=layout)
     return fig
 
-app = dash.Dash(__name__)
-server = app.server
+server = Flask(__name__)
+
+app = dash.Dash(name =__name__, server=server)
 app.layout = html.Div(children=[
     html.H1(children='Demo',
             style={'textAlign': 'center'}
